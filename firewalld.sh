ac75bbb4cde8dbc23ca8ddb763ce101b0c62a57c
ketfree <96343945+ketfree@users.noreply.github.com>
2025-01-06 09:37:36 -0500

    firewalld.sh

--- firewalld.sh
+++ firewalld.sh
@@ -16,15 +16,15 @@ iptables-save > /etc/iptables/rules.v4
 # Instalar firewalld
 apt install -y firewalld
 
+# Iniciar y habilitar firewalld
+systemctl start firewalld
+systemctl enable firewalld
+
 # Abrir puertos TCP
-iptables -t nat -A DOCKER -p tcp -d 127.0.0.1 --dport 15888 -j DNAT --to-destination 172.17.0.4:15888 ! -i docker0
-firewall-cmd --add-port=15888/tcp --permanent
-firewall-cmd --add-port=50051/udp --permanent
-firewall-cmd --add-port=22/tcp --permanent
-firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --add-port=443/tcp --permanent
-firewall-cmd --add-port=30088/tcp --permanent
 firewall-cmd --add-port=30001-30005/tcp --permanent
+firewall-cmd --add-port=30010-30011/tcp --permanent
+firewall-cmd --add-port=30020-30021/tcp --permanent
 firewall-cmd --add-port=32768-65535/tcp --permanent
 
 # Abrir puertos UDP
